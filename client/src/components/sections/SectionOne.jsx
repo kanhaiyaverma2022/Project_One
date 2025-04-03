500b201d0c103935819da10f831137a65bb1a1a0
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2025-04-03 22:28:21 +0530

    setup completed

---
+++ client/src/components/sections/SectionOne.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SectionOne = ({title}) => {
+
+
+  return (
+    <>
+      <h3>{title}</h3>
+      
+    </>
+  )
+}
+
+export default SectionOne
---
+++ client/src/components/sections/SectionTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SectionTwo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SectionTwo
+
