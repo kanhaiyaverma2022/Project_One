210b0f617d62c829373b256dff67976184038ec4
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2025-04-05 12:05:34 +0530

    minor issues resolved

--- client/src/reusableComponents/Containers.jsx
+++ client/src/reusableComponents/Containers.jsx
@@ -1,6 +1,6 @@
 const OuterLayout = ({ children }) => {
     return (
-      <div className="px-[30px] border border-black md:px-[60px] py-[50px] md:py-[100px] flex flex-col gap-y-10 md:gap-y-20">
+      <div className="px-[30px]  md:px-[60px] py-[50px] md:py-[100px] flex flex-col gap-y-10 md:gap-y-20">
         {children}
       </div>
     );
@@ -10,13 +10,13 @@ const OuterLayout = ({ children }) => {
 const CardLayout = ({image,title,width}) =>{
 
   return(
-    <div className={`${width} flex flex-col justify-center items-center h-[494px]`}>
+    <div className={`${width} flex flex-col justify-center items-center h-auto md:h-[494px]`}>
   <div className='w-full  p-[15px] bg-neutral-pink rounded-[30px]'>
-    <img  src={image} className="w-full h-[383px]  object-fill rounded-[30px]"/>
+    <img  src={image} className="w-full h-[300px] md:h-[383px]  object-fill rounded-[30px]"/>
   </div>
   <div className="w-full flex justify-center">
-  <div className="px-8 bg-neutral-pink w-fit rounded-b-[30px] h-20 flex justify-center items-center">
-    <p className="font-montserrat text-center text-lg md:text-2xl font-semibold">
+  <div className="py-2 px-8 lg:px-4 xl:px-8 bg-neutral-pink w-fit rounded-b-[30px] h-auto flex ">
+    <p className=" lg:max-w-[250px] max-w-[300px]  text-center h-[32px] lg:h-[63px] font-montserrat flex items-center justify-center text-base lg:text-base  xl:text-xl font-semibold">
       {title}
     </p>
   </div>
